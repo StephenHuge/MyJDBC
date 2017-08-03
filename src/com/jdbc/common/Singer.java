36f26c43975998639de3bfd9fb499cae1e8bc3ef
HJS <407410014@qq.com>
2017-08-03 23:15:46 +0800

    add POJO classes 'User' and 'Singer'

---
+++ src/com/jdbc/common/Singer.java
@@ -0,0 +1,55 @@
+package com.jdbc.common;
+
+public class Singer {
+	private int id;
+	private String name;
+	private String bestSong;
+	
+	
+	public Singer() {
+	}
+
+
+	public Singer(String name, String bestSong) {
+		this.name = name;
+		this.bestSong = bestSong;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getBestSong() {
+		return bestSong;
+	}
+
+
+	public void setBestSong(String bestSong) {
+		this.bestSong = bestSong;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Singer [id=" + id + ", name=" + name + ", bestSong=" + bestSong + "]";
+	}
+	
+	
+}
