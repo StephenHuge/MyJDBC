65dd6d83ce3617097025dc00e04fb431c500b101
HJS <407410014@qq.com>
2017-08-06 01:52:25 +0800

    complete method myWriteBlob()

--- src/com/jdbc/extensions/CommonUpdateBlob.java
+++ src/com/jdbc/extensions/CommonUpdateBlob.java
@@ -1,7 +1,38 @@
 package com.jdbc.extensions;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.junit.Test;
+
+import com.jdbc.mytools.MyJDBCTools;
+
 public class CommonUpdateBlob {
-	public void myWriteBlob() {}
 	
-	public void myReadBlob() {}
+	@Test
+	public void myWriteBlob() {
+		
+		Connection connection = null;
+		
+		String sql = "UPDATE singer SET avatar = ? WHERE name = ?";
+		try {
+			connection = MyJDBCTools.getConnection();
+			QueryRunner qr = new QueryRunner();
+			
+			InputStream inStream = new FileInputStream("Jolin Cai.jpg");
+			
+			qr.update(connection, sql, inStream, "Jolin Cai");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			MyJDBCTools.releaseDB(connection);
+		}
+	}
+	
+	public void myReadBlob() {
+		
+	}
 }
